c3665c7a95336294a83b5c712c1f6ade0adbaf18
Jeetendra Kashyap <kashyapjeetendra@gmail.com>
2019-06-21 12:07:32 +0530

    Created category controller with placeholder functions

---
+++ app/controllers/category.js
@@ -0,0 +1,23 @@
+const Category = require('../models/category.js');
+
+exports.create = (req, res) => {
+
+};
+
+
+exports.findAll = (req, res) => {
+
+};
+
+exports.findOne = (req, res) => {
+
+};
+
+exports.update = (req, res) => {
+
+};
+
+
+exports.delete = (req, res) => {
+
+};
